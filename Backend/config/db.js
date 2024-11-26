0417d8b186242022cfb6cee703d7d09b0d777d6b
SuvidhiSingh <120492855+SuvidhiSingh@users.noreply.github.com>
2024-11-26 20:18:31 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://suvidhi_4:987623416@cluster1.udzftim.mongodb.net/Athletes_Arsenal').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 }
 
